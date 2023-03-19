323b79f393b26fa7b79cd0f3d8602e7ad3a5a0c4
Thomas Josef Dorfer <2110738536@stud.fh-kufstein.ac.at>
2023-03-19 16:22:57 +0100

    updated rnd number array function

--- src/functions/rnd-number-array.function.ts
+++ src/functions/rnd-number-array.function.ts
@@ -3,10 +3,7 @@ import { rndNumber } from "./rnd-number.function";
 export function rndNumberArray(size = 1, max = 1, min = 0): number[] {
     const arr = []
     while (arr.length < size) {
-        const v = rndNumber(max);
-        if (v >= min) {
-            arr.push(v);
-        }
+        arr.push(rndNumber(max, min));
     }
     return arr;
 }
