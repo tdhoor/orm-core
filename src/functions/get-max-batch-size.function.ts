340af3c4785dc6ec4a0032bb8ae59783a1cafeea
tdhoor <info@thomasdorfer.com>
2023-04-08 21:21:16 +0200

    updated max batch size

--- src/functions/get-max-batch-size.function.ts
+++ src/functions/get-max-batch-size.function.ts
@@ -1,7 +1,7 @@
 export function getMaxBatchSize(recordsToInsert) {
     //source https://github.com/knex/knex/issues/2986#issuecomment-1129987488
     //mssql supports a max of 2100 parameters in one statement
-    const maxDBParameters = 2100;
+    const maxDBParameters = 2100 - 10
     //get a set of all column names, across all the records
     const uniqueKeySet = recordsToInsert
         .reduce((uniqueKeySet, currentRecord) => {
