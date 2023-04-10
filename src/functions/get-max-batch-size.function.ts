bc5cbd7c5a18052f348c27fecd2b7705385444a5
tdhoor <info@thomasdorfer.com>
2023-04-10 17:52:37 +0200

    fixed mssql insert max row error

--- src/functions/get-max-batch-size.function.ts
+++ src/functions/get-max-batch-size.function.ts
@@ -3,12 +3,11 @@ export function getMaxBatchSize(recordsToInsert) {
     //mssql supports a max of 2100 parameters in one statement
     const maxDBParameters = 2100 - 10
     //get a set of all column names, across all the records
-    const uniqueKeySet = recordsToInsert
-        .reduce((uniqueKeySet, currentRecord) => {
-            Object
-                .keys(currentRecord)
-                .forEach(key => uniqueKeySet.add(key));
-            return uniqueKeySet;
-        }, new Set())
-    return Math.floor(maxDBParameters / uniqueKeySet.size)
+    const uniqueKeySet = new Set();
+    Object
+        .keys(Array.isArray(recordsToInsert) ? recordsToInsert[0] : recordsToInsert)
+        .forEach(key => uniqueKeySet.add(key));
+    const maxBatchSize = Math.floor(maxDBParameters / uniqueKeySet.size)
+    //mssql supports a max of 1000 records in one statement, else ERROR 10738
+    return maxBatchSize > 1000 ? 1000 : maxBatchSize;
 }
