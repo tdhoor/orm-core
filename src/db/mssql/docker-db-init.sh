2058e12f1706582fb77d2c93730da87d288bdded
tdhoor <info@thomasdorfer.com>
2023-04-10 23:30:17 +0200

    fixed sleep error in setup script

--- src/db/mssql/docker-db-init.sh
+++ src/db/mssql/docker-db-init.sh
@@ -1,6 +1,1 @@
-#wait for the SQL Server to come up
-sleep 30s
-
-echo "Running set up script"
-#run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P a1?%fkARF!24x -d master -i db-init.sql
+sleep 28 && echo "Running set up script" && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P a1?%fkARF!24x -d master -i db-init.sql
