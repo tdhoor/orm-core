365322c912bb9b00ccb9e6c85341aef115ef4ccc
tdhoor <info@thomasdorfer.com>
2023-04-10 14:33:34 +0200

    added bulk insert function for mysql

---
+++ src/functions/bulk-insert-mysql.function.ts
@@ -0,0 +1,22 @@
+function bulkInsertMysql<T>(pool, table: string, values: T[]): Promise<void> {
+    const keys = Object.keys(values[0]);
+    const tableName = "`" + table + "`";
+    const query = `INSERT INTO ${tableName} (${keys.join(', ')}) VALUES ?`;
+    const rows = values.map((value) => Object.values(value));
+    return new Promise<void>((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                reject(err);
+            } else {
+                connection.query(query, [rows], (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                    connection.release();
+                });
+            }
+        });
+    });
+}
